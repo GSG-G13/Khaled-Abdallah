8e43d7411b0966506cdac48c01506947e81c1cae
AbdallahAbujazar <abdallah.abujazar@iths.se>
2023-05-17 14:04:02 +0300

    TenTen

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -1,25 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from './Card';
 
-class CardList extends React.Component {
-  state = {
-    data: null,
-    page: 1,
-    url: `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&sort_by=popularity.desc&api_key=b9da8a8928ade30c5680978edd9a4330&page=`,
-    filter: ''
-  };
-now_playing
-  pages = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  filterList = ['Now Playing', 'Popular', 'Top Rated', 'Upcoming']
-  
+class CardList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      page: 1,
+      url: 'https://api.themoviedb.org/3/discover/movie',
+      filter: '',
+      genres: [],
+      searchQuery: '',
+      selectedCategory: '',
+    };
+    this.pages = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    this.filterList = ['Now Playing', 'Popular', 'Top Rated', 'Upcoming']
+
+  }
+
   componentDidMount() {
+    this.fetchGenres();
     this.fetchData();
   }
 
+  fetchGenres = async () => {
+    const apiKey = 'b9da8a8928ade30c5680978edd9a4330';
+    const apiUrl = `https://api.themoviedb.org/3/genre/movie/list?include_adult=false&api_key=${apiKey}`;
+
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      this.setState({ genres: data.genres });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  changelist = (selectedCategory) => {
+    this.setState(
+      {
+        selectedCategory,
+        page: 1,
+      },
+      () => {
+        this.fetchData();
+      }
+    );
+  };
+  
+
+  fetchData = async () => {
+    const { url, filter, page, searchQuery, selectedCategory } = this.state;
+    const apiKey = 'b9da8a8928ade30c5680978edd9a4330';
+    let apiUrl = `${url}?include_adult=false&api_key=${apiKey}&language=en-US&page=${page}&with_genres=${filter}`;
+
+    if (searchQuery) {
+      apiUrl = `https://api.themoviedb.org/3/search/movie?include_adult=false&query=${searchQuery}&api_key=b9da8a8928ade30c5680978edd9a4330&page=${page}`;
+    } else if (selectedCategory) {
+      apiUrl = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&sort_by=popularity.desc&api_key=b9da8a8928ade30c5680978edd9a4330&page=${page}&with_genres=${selectedCategory}`;
+    } else {
+      apiUrl = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&sort_by=popularity.desc&api_key=b9da8a8928ade30c5680978edd9a4330&page=${page}`;
+    }
+
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      this.setState({ data: data.results });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
   changeFilter(filter){
     this.setState({ filter: filter }, ()=> {
       console.log(this.state.filter);
-      this.setState({ url : `https://api.themoviedb.org/3/movie/${this.state.filter}?api_key=b9da8a8928ade30c5680978edd9a4330&language=en-US&page=`}, ()=> {
+      this.setState({ url : `https://api.themoviedb.org/3/movie/${this.state.filter}?include_adult=false&api_key=b9da8a8928ade30c5680978edd9a4330&language=en-US&page=`}, ()=> {
         console.log(this.filterUrl);
         console.log(this.state.url);
         this.fetchData()
@@ -27,41 +82,74 @@ now_playing
       
     })
   }
-
-  fetchData() {
-    fetch(this.state.url+this.state.page)
-      .then(data => data.json())
-      .then(data => {
-        this.setState({ data: data.results });
-      });
-  }
-
   changePage = (page) => {
     this.setState({ page }, () => {
       this.fetchData();
     });
   };
 
+  handleSearch = (event) => {
+    this.setState({ searchQuery: event.target.value });
+  };
+
+
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ page: 1 }, () => {
+      this.fetchData();
+    });
+  };
+
+
   render() {
-    const { data } = this.state;
+    const { data, genres , searchQuery} = this.state;
 
     return (
-      !data ? <div>Loading...</div> :
-        <main>
-          <div className="filter">
+      <main>
+        <div className="filter ">
+        <input
+              type="text"
+              value={searchQuery}
+              onChange={this.handleSearch}
+              placeholder="Search movies"
+              className='catList'
+            />          
+            <button onClick={this.handleSubmit} className='catList search'>Search</button>
+         
+          <select onChange={(e) => this.changelist(e.target.value)} className='catList'>
+            <option value="">All Categories</option>
+            {genres.map((genre) => (
+              <option key={genre.id} value={genre.id}>
+                {genre.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <br />
+        <div className="filter">
             {this.filterList.map(filter => <div key={filter} onClick={()=> this.changeFilter(filter.toLowerCase().replace(' ', '_'))}>{filter}</div>)}
           </div>
+        <div className="cards-grid">
+          {data ? (
+            data.map((card) => <Card key={card.id} data={card} />)
+          ) : (
+            <div>Loading...</div>
+          )}
+        </div>
 
-          <div className="cards-grid">
-            {data.map(card => <Card key={card.id} data={card} />)}
-          </div>
-          
-          <ul className="pages-list">
-            {this.pages.map(page => (
-              <li className='page' key={page} onClick={() => this.changePage(page)}>{page}</li>
-            ))}
-          </ul>
-        </main>
+        <ul className="pages-list">
+          {this.pages.map((page) => (
+            <li
+              className="page"
+              key={page}
+              onClick={() => this.changePage(page)}
+            >
+              {page}
+            </li>
+          ))}
+        </ul>
+      </main>
     );
   }
 }
